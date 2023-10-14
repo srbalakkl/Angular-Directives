dca9e463e50e0c5228d1035cce28f2eac70486c9
sr.bala <sr.bala.kkl@gmail.com>
2023-10-14 08:38:05 +0530

    NEW: NgTemplateOutlet with Context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,13 @@ export class AppComponent {
 
   ary: any = ['asdf', 1, 'sdf'];
 
-  myArticle: { id: number, name: string }[] = [
-    {"id": 0, "name": "Available"},
-    {"id": 1, "name": "Ready"},
-    {"id": 2, "name": "Started"}
-  ];
+  // myArticle: { id: number, name: string }[] = [
+  //   {"id": 0, "name": "Available"},
+  //   {"id": 1, "name": "Ready"},
+  //   {"id": 2, "name": "Started"}
+  // ];
+
+  myContext = {$implicit: 'World', localSk: 'Svet'};
+
+  checkVal: string = 'Bala';
 }
