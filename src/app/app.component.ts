4b5396363a63cd970719b17ccd5022bcf55597d1
bala <sr.bala.kkl@gmail.com>
2025-06-11 17:24:04 +0530

    IMPROVE: myArticle Doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
 
   ary: any = ['asdf', 1, 'sdf'];
 
-  // myArticle: { id: number, name: string }[] = [
-  //   {"id": 0, "name": "Available"},
-  //   {"id": 1, "name": "Ready"},
-  //   {"id": 2, "name": "Started"}
-  // ];
+  myArticle: { id: number, name: string }[] = [
+    {"id": 0, "name": "Available"},
+    {"id": 1, "name": "Ready"},
+    {"id": 2, "name": "Started"}
+  ];
 
-  myContext = {$implicit: 'World', localSk: 'Svet'};
+  myContext = {$implicit: 'World', localSk: 'Riya'};
 
   checkVal: string = 'Bala';
 }
